3229261148631df850c2ff923b68ee5b321070a6
Yash Prasad <yashlucky@Yashs-MacBook-Pro.local>
2024-08-18 18:51:22 +0530

    Prepared Base model

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ sys.path.append(dir_2)
 
 from src.cdclassifier import logger
 from src.cdclassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.cdclassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -23,3 +24,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = " Prepare Base Model Stage" 
+try:
+    logger.info(f'>>>>>> stage {STAGE_NAME} started <<<<<<')
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f'>>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x')
+
+except Exception as e:
+    logger.exception(e)
+    raise e 
--- src/cdclassifier/pipeline/stage_02_prepare_base_model.py
+++ src/cdclassifier/pipeline/stage_02_prepare_base_model.py
@@ -1,25 +1,25 @@
 from cdclassifier.config.configuration import ConfigurationManager
-from cdclassifier.components.data_ingestion import DataIngestion
+from cdclassifier.components.prepare_base_model import PrepareBaseModel
 from cdclassifier import logger
 
-STAGE_NAME = "Data Ingestion Stage"
+STAGE_NAME = " Prepare Base Model Stage" 
 
-class DataIngestionTrainingPipeline:
+class PrepareBaseModelTrainingPipeline:
     def __init__(self):
         pass
 
     def main(self):
         config = ConfigurationManager()
-        data_ingestion_config = config.get_data_ingestion_config()
-        data_ingestion = DataIngestion(config = data_ingestion_config)
-        data_ingestion.download_file()
-        data_ingestion.extract_zip_file()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config= prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
 
 
 if __name__ == '__main__':
     try:
         logger.info(f'>>>>>> stage {STAGE_NAME} started <<<<<<')
-        obj = DataIngestionTrainingPipeline()
+        obj = PrepareBaseModelTrainingPipeline()
         obj.main()
         logger.info(f'>>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x')
     
